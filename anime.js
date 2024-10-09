69f28aec2c1853d27f8c19c0858c30a4ed677ea9
dylanR96 <dylan.ravenson@gmail.com>
2024-10-09 11:35:53 +0200

    Completed exercise 6,7, and 8

--- anime.js
+++ anime.js
@@ -1,4 +1,8 @@
 const box = document.querySelector(".box");
+let path = anime.path(".svgPath");
+const text = "Magic text that types itself";
+const letters = text.split("");
+const sentence = document.querySelector(".text-container");
 
 box.addEventListener("click", function () {
   anime({
@@ -11,13 +15,7 @@ box.addEventListener("click", function () {
 
 anime({
   targets: ".spinner",
-  keyframes: [
-    { rotate: "0deg", backgroundColor: "green" },
-    { backgroundColor: "blue" },
-    { rotate: "180deg", backgroundColor: "red" },
-    { backgroundColor: "yellow" },
-    { rotate: "360deg", backgroundColor: "green" },
-  ],
+  keyframes: [{ rotate: "0deg" }, { rotate: "180deg" }, { rotate: "360deg" }],
   duration: 500,
   easing: "linear",
   loop: true,
@@ -41,3 +39,51 @@ anime({
   easing: "linear",
   loop: true,
 });
+
+anime({
+  targets: ".movingBox",
+  translateX: path("x"),
+  translateY: path("y"),
+  rotate: path("angle"),
+  easing: "linear",
+  duration: 2000,
+  loop: true,
+});
+
+let circle_animations = anime.timeline({
+  easing: "easeOutExpo",
+  duration: 750,
+  loop: true,
+});
+
+circle_animations
+  .add({
+    targets: ".circle-one",
+    translateX: -250,
+  })
+  .add({
+    targets: ".circle-two",
+    translateY: -250,
+  })
+  .add({
+    targets: ".circle-three",
+    translateX: 250,
+  });
+
+letters.forEach((letter) => {
+  let span = document.createElement("span");
+  span.innerHTML = letter === " " ? "&nbsp;" : letter;
+  span.classList.add("letter");
+  sentence.appendChild(span);
+});
+
+anime({
+  targets: ".letter",
+  opacity: [0, 1],
+  easing: "easeInOutQuad",
+  delay: anime.stagger(100),
+  duration: 100,
+});
+
+const userText = document.querySelector("users-input");
+const textContainer = document.querySelector("typewriter-text");
